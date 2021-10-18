3a0759d61108690765b9ac06144f01fa982e6711
Sumitha <sumitha.ela23@gmail.com>
2021-10-18 11:32:05 +1100

    Updating the cart section when AddToCart btn clicked

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const productsDiv = document.querySelector(".products");
+const cartItemsDiv = document.querySelector(".cart-items");
 
-//render array of products object
+//render array of "products" object from "products.js"
 function render(){
     products.forEach((product) => {
         productsDiv.innerHTML += `
@@ -11,7 +12,7 @@ function render(){
           <h5 class="card-title">${product.name}</h5>
           <p class="card-text"><small>$${product.price}</small></p>
           <div class="add-to-cart">
-          <button type="button" class="btn btn-primary mt-2" onClick="addToCart(${product.id})">Add to cart</button>
+          <button type="button" class="btn mt-2" onClick="addToCart(${product.id})">Add to cart</button>
           </div>
         </div>
         </div>
@@ -22,20 +23,45 @@ function render(){
 
 render();
 
+//add to the cart
 let cart = [];
 function addToCart(id) {
-//  console.log(id);
+ console.log(id);
 if(cart.some((item) => item.id === id)) {
     alert("Product already in the cart!");
 } else {
     const item = products.find((product) => product.id === id);
     // console.log(item);
-    cart.push({
-        ...item,
-        numberOfUnits : 1
-    });
-    console.log("Cart Array..", cart);
-}
-    
+    cart.push(item);      
+  
+    // console.log("Cart Array..", cart);
 }
 
+ updateCart();
+}
+
+//updating the cart section
+function updateCart() {
+    renderCartItems();
+}
+
+//rendering the cart section with selected items
+function renderCartItems() {
+    cartItemsDiv.innerHTML = "";
+    cart.forEach((item) => {
+        cartItemsDiv.innerHTML += `
+            <div class="cart-item">
+            <div class="item-info">
+                <h4>${item.name}</h4>
+            </div>
+            <div class="unit-price">
+                <small>$</small>${item.price}
+            </div>
+            <div class="units">
+                <div class="number">${item.quantity}</div>
+            </div>
+            </div>
+        `;
+    });
+
+}
--- js/products.js
+++ js/products.js
@@ -4,17 +4,20 @@ const products = [
     name: "Desktop",
     price: 2000,
     imgSrc: "./images/desktop.jpg",
+    quantity: 1,
     },
     {
     id: 1,
     name: "Laptop",
     price: 1200,
     imgSrc: "./images/laptop.jpg",
+    quantity: 1,
     },
     {
     id: 2,
     name: "Tablet",
     price: 2000,
     imgSrc: "./images/tablet.jpg",
+    quantity: 1,
     },
 ];
